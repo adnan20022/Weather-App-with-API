dd7ca93578d2b0d457b6d5709409ee6db4db9351
adnan20022 <mu209367@gmail.com>
2025-05-09 22:23:03 -0700

    styled weather-app

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 async function getWeather() {
   const city = document.getElementById("cityInput").value;
-  const apiKey = "e1601e0b298d82d3d650de1b8638430d"; //  OpenWeatherMap API key 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  const url = `https://p2pclouds.up.railway.app/v1/learn/weather?city=${encodeURIComponent(city)}`;
 
   try {
-    const response = await fetch(url);
+    const response = await fetch(url,{
+      method: "Get",
+    });
     const data = await response.json();
 
-    if (data.cod === 200) {
+    if (response.ok && data.location && data.current) {
       document.getElementById("weatherResult").innerHTML = `
-        <h2>${data.name}, ${data.sys.country}</h2>
-        <p>🌡 Temperature: ${data.main.temp} °C</p>
-        <p>☁ Weather: ${data.weather[0].description}</p>
-        <p>💨 Wind Speed: ${data.wind.speed} m/s</p>
+        <h2>${data.location.name}, ${data.location.country}</h2>
+        <p>🌡 Temperature: ${data.current.temp_c} °C</p>
+        <p>☁ Weather: ${data.current.condition.text}</p>
+        <p>💨 Wind Speed: ${data.current.wind_kph} kph</p>
       `;
     } else {
       document.getElementById("weatherResult").innerHTML = `<p>City not found.</p>`;
     }
   } catch (error) {
+    console.error("Fetch error:", error);
     document.getElementById("weatherResult").innerHTML = `<p>Error fetching data.</p>`;
   }
 }
